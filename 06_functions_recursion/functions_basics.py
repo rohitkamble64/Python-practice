9254d6f1d05e3c44a4b3f2d85bb8d43f01b93c65
Rohit Kamble <rohitkamble6405@gmail.com>
2026-08-05 14:57:33 +0530

    Add Python function basics

---
+++ 06_functions_recursion/functions_basics.py
@@ -0,0 +1,15 @@
+def greet(name, end):
+    print("Good Afternoon, " + name)
+    print(end)
+
+greet("Rohit", "Have a great day!") # Function Call with argument
+greet("Sam", "Thanks") 
+
+#function definition 
+def avg():
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    average = (a+b)/2
+    print("Average is ", average) 
+
+avg()  # Functions Call
