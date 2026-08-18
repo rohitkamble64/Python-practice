5d054a2f410878cf46678e5aaa93b53fcb6a39a1
Rohit Kamble <rohitkamble6405@gmail.com>
2026-08-18 23:47:43 +0530

    Add Type definitions programs

---
+++ 11_type_definitions/02_type_definitions.py
@@ -0,0 +1,9 @@
+#Variable Type Hint
+age: int = 21
+
+# Function type hint
+def Greet(name: str) -> str:
+    return f"Hello {name}"
+
+print(Greet("Rohit"))
+
---
+++ 11_type_definitions/03_types.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple, Dict
+
+#List of integers
+numbers: list[int] = [1, 2, 3, 4, 5]
+
+#Tuple of string and an integer
+person: tuple[str, int] = ("Rohit", 67)
+
+#Dictionary with string keys and integer values
+scores: dict[str, int] = {"Rohan" : 93, "John": 83}
+
+print(numbers)
+print(person)
+print(scores)
